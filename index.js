0b9ddfb4dfaeb115ec23b36c07bbc08b9bde5461
Dazed31 <darrenamore13@gmail.com>
2026-03-27 13:00:11 +0300

    Made functions as directed by the lab assigneng given by our technical mentor

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+function calculateTax(amount){
+ return amount * 0.10;
+}
 
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
+
+function findMaximum(num1, num2){
+    if (num1 > num2){
+        return num1;
+    }
+
+    else{
+        return num2;
+    }
+}
+
+function isPalindrome(word){
+   let reversed = word.split('').reverse().join('');
+   return word === reversed;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+    let discount = (discountPercentage/100) * originalPrice;
+    let finalPrice = originalPrice - discount;
+
+    return finalPrice;
+}
 
 
 
